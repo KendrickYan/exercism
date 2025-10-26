25026aeb61700663f5240b9b381016c574f9b0bf
Kendrick ACER <kendrickyanuar@gmail.com>
2025-10-26 12:25:25 +1100

    done log-levels exercise, iteration 1, exemplar solution, but check out this guy's solution https://exercism.org/tracks/java/exercises/log-levels/solutions/AndrzejProchyra

--- java/log-levels/src/main/java/LogLevels.java
+++ java/log-levels/src/main/java/LogLevels.java
@@ -1,14 +1,22 @@
 public class LogLevels {
-    
+
     public static String message(String logLine) {
-        throw new UnsupportedOperationException("Please implement the (static) LogLevels.message() method");
+        String[] logLine_arr = logLine.split(":\\s+");
+        String log_message = logLine_arr[logLine_arr.length - 1].stripTrailing();
+        return log_message;
     }
 
     public static String logLevel(String logLine) {
-        throw new UnsupportedOperationException("Please implement the (static) LogLevels.logLevel() method");
+        String[] logLine_arr = logLine.split(":\\s+");
+        String log_level = logLine_arr[0].toLowerCase();
+        log_level = log_level.substring(1, log_level.length() - 1);
+        return log_level;
     }
 
     public static String reformat(String logLine) {
-        throw new UnsupportedOperationException("Please implement the (static) LogLevels.reformat() method");
+        String log_message = message(logLine);
+        String log_level = logLevel(logLine);
+        String reformated_message = log_message + " (" + log_level + ")";
+        return reformated_message;
     }
 }
