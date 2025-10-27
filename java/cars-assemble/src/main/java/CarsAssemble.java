8f5b62cc136501c5daeea50850af0367523f3885
Kendrick ACER <kendrickyanuar@gmail.com>
2025-10-27 11:40:05 +1100

    done "Cars, Assemble!" exercise, iteration 1, no auto suggestions
    lowkey strange that i defaulted it to return 0 but wtv

--- java/cars-assemble/src/main/java/CarsAssemble.java
+++ java/cars-assemble/src/main/java/CarsAssemble.java
@@ -1,10 +1,24 @@
 public class CarsAssemble {
 
+    private final int BASE_PRODUCTION_RATE = 221; // cars per hour
+
     public double productionRatePerHour(int speed) {
-        throw new UnsupportedOperationException("Please implement the CarsAssemble.productionRatePerHour() method");
+        int rate = speed * BASE_PRODUCTION_RATE;
+
+        if (1 <= speed && speed <= 4) {
+            return rate;
+        } else if (5 <= speed && speed <= 8) {
+            return rate * 0.9;
+        } else if (speed == 9) {
+            return rate * 0.8;
+        } else if (speed == 10) {
+            return rate * 0.77;
+        }
+
+        return 0;
     }
 
     public int workingItemsPerMinute(int speed) {
-        throw new UnsupportedOperationException("Please implement the CarsAssemble.workingItemsPerMinute() method");
+        return (int) (productionRatePerHour(speed) / 60);
     }
 }
