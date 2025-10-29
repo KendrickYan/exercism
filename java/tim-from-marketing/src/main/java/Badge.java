c937688625c5ea9b914917d342ec327f8c555b8e
Kendrick ACER <kendrickyanuar@gmail.com>
2025-10-29 17:13:41 +1100

    done "Tim from Marketing" exercise, iteration 1, auto feedback says dont use String.format. It is extremely slow for problems where simple concatenation is possible.

--- java/tim-from-marketing/src/main/java/Badge.java
+++ java/tim-from-marketing/src/main/java/Badge.java
@@ -1,5 +1,13 @@
 class Badge {
     public String print(Integer id, String name, String department) {
-        throw new UnsupportedOperationException("Please implement the Badge.print() method");
+        StringBuilder sb = new StringBuilder();
+
+        if (id != null) sb.append(String.format("[%d] - ", id));
+
+        sb.append(name)
+                .append(" - ")
+                .append(department == null ? "OWNER" : department.toUpperCase());
+
+        return sb.toString();
     }
 }
