0555c185d303760749261a81aaaf7b8533fa5eb3
Kendrick Macbook <kendrickyanuar@gmail.com>
2025-11-17 13:28:20 +0800

    done "Wizards and Warriors" exercise, iteration 1 (2 on the website)
    put all the classes in the given class for submission

--- java/wizards-and-warriors/src/main/java/Fighter.java
+++ java/wizards-and-warriors/src/main/java/Fighter.java
@@ -9,6 +9,44 @@ class Fighter {
     }
 }
 
-// TODO: define the Warrior class
+class Warrior extends Fighter {
 
-// TODO: define the Wizard class
+    @Override
+    public String toString() {
+        return "Fighter is a Warrior";
+    }
+
+    @Override
+    public boolean isVulnerable() {
+        return false;
+    }
+
+    @Override
+    public int getDamagePoints(Fighter fighter) {
+        return fighter.isVulnerable() ? 10 : 6;
+    }
+}
+
+class Wizard extends Fighter {
+
+    private boolean spellPrepared = false;
+
+    @Override
+    public String toString() {
+        return "Fighter is a Wizard";
+    }
+
+    public void prepareSpell() {
+        spellPrepared = true;
+    }
+
+    @Override
+    public boolean isVulnerable() {
+        return !spellPrepared;
+    }
+
+    @Override
+    public int getDamagePoints(Fighter fighter) {
+        return spellPrepared ? 12 : 3;
+    }
+}
