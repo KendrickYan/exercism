4c7ac6d1c86fc33936afc2b969e629d09daa054d
Kendrick Macbook <kendrickyanuar@gmail.com>
2025-11-21 09:29:10 +0800

    done "Calculator Conundrum" exercise, iteration 1
    idk who added line 1 but it's unused

--- java/calculator-conundrum/src/main/java/CalculatorConundrum.java
+++ java/calculator-conundrum/src/main/java/CalculatorConundrum.java
@@ -1,5 +1,32 @@
+import javax.xml.crypto.dsig.spec.XSLTTransformParameterSpec;
+
 class CalculatorConundrum {
     public String calculate(int operand1, int operand2, String operation) {
-        throw new UnsupportedOperationException("Please implement the CalculatorConundrum.calculate() method");
+
+        if (operation == null) {
+            throw new IllegalArgumentException("Operation cannot be null");
+        }
+
+        // using elif here is weird coz we know that the program will stop at throw in line 7
+        if (operation.isEmpty()) {
+            throw new IllegalArgumentException("Operation cannot be empty");
+        }
+
+        return switch (operation) {
+            case "+" -> format(operand1, operand2, operation, (operand1 + operand2));
+            case "*" -> format(operand1, operand2, operation, (operand1 * operand2));
+            case "/" -> {
+                try {
+                    yield format(operand1, operand2, operation, (operand1 / operand2));
+                } catch (ArithmeticException e) {
+                    throw new IllegalOperationException("Division by zero is not allowed", e);
+                }
+            }
+            default -> throw new IllegalOperationException("Operation '" + operation + "' does not exist");
+        };
+    }
+
+    private String format(int operand1, int operand2, String operation, int result) {
+        return operand1 + " " + operation + " " + operand2 + " = " + result;
     }
 }
