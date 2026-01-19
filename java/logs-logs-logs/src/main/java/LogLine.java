8f4b96d0386900cf2dd70cd937d7b46fe0ad3f90
Kendrick Macbook <kendrickyanuar@gmail.com>
2026-01-19 20:48:48 +1100

    done "Logs, Logs, Logs!" exercise, iteration 1
    valuable example for enums

--- java/logs-logs-logs/src/main/java/LogLine.java
+++ java/logs-logs-logs/src/main/java/LogLine.java
@@ -1,13 +1,27 @@
 public class LogLine {
 
+    private final String levelToken;
+    private final String message;
+
     public LogLine(String logLine) {
+        String[] splitLogLine = logLine.split("]: ");
+        this.levelToken = splitLogLine[0].substring(1);
+        this.message = splitLogLine[1];
     }
 
     public LogLevel getLogLevel() {
-        throw new UnsupportedOperationException("Please implement the getLogLevel() method");
+        return switch (levelToken) {
+            case "TRC" -> LogLevel.TRACE;
+            case "DBG" -> LogLevel.DEBUG;
+            case "INF" -> LogLevel.INFO;
+            case "WRN" -> LogLevel.WARNING;
+            case "ERR" -> LogLevel.ERROR;
+            case "FTL" -> LogLevel.FATAL;
+            default -> LogLevel.UNKNOWN;
+        };
     }
 
     public String getOutputForShortLog() {
-        throw new UnsupportedOperationException("Please implement the getOutputForShortLog() method");
+        return getLogLevel().getCode() + ":" + message;
     }
 }
--- java/runner-test/Runner.java
+++ java/runner-test/Runner.java
@@ -1,16 +1,14 @@
 // powershell> javac Runner.java; java Runner
 // cmd> javac Runner.java & java Runner
+// bash> javac Runner.java & java Runner
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class Runner {
     public static void main(String[] args) {
-        String someString = "Hello";
-        char[] toArray = someString.toCharArray();
-
-        for (char ch: toArray) {
-            System.out.println(ch);
-        }
+        String someString = "[Hello:World!";
+        String[] toArray = someString.split(":");
+        System.out.println(toArray[0].substring(1));
     }
 }
