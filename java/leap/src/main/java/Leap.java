d6ffeaffae20d4e78fa379ea5ad1594c3736df54
Kendrick Macbook <kendrickyanuar@gmail.com>
2026-02-10 17:30:28 +1100

    done leap exercise, iteration 3, compressed all into 1 line

--- java/leap/src/main/java/Leap.java
+++ java/leap/src/main/java/Leap.java
@@ -1,8 +1,7 @@
 class Leap {
 
     boolean isLeapYear(int year) {
-        if (year % 100 == 0) return year % 400 == 0;
-        return year % 4 == 0;
+        return (year % 4 == 0 && year % 100 != 0) || year % 400 == 0;
     }
 
 }
