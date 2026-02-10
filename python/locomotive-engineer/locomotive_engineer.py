5bb4e3aa38558ab571414c450942279a4656e4c9
Kendrick Macbook <kendrickyanuar@gmail.com>
2026-02-10 13:27:22 +1100

    done locomotive engineer exercise

--- python/locomotive-engineer/locomotive_engineer.py
+++ python/locomotive-engineer/locomotive_engineer.py
@@ -1,49 +1,54 @@
 """Functions which helps the locomotive engineer to keep track of the train."""
 
 
-def get_list_of_wagons():
+def get_list_of_wagons(*args) -> list[int]:
     """Return a list of wagons.
 
     :param: arbitrary number of wagons.
     :return: list - list of wagons.
     """
-    pass
+    return list(args)
 
 
-def fix_list_of_wagons(each_wagons_id, missing_wagons):
+def fix_list_of_wagons(each_wagons_id: list[int], missing_wagons: list[int]) -> list[int]:
     """Fix the list of wagons.
 
     :param each_wagons_id: list - the list of wagons.
     :param missing_wagons: list - the list of missing wagons.
     :return: list - list of wagons.
     """
-    pass
+    # so 1, missing_wagons, wagons after 1, wagons before 1
 
+    index_of_1 = each_wagons_id.index(1)
+    earlier_wagons = each_wagons_id[:index_of_1]
+    _, *later_wagons = each_wagons_id[index_of_1:]
+    return [1, *missing_wagons, *later_wagons, *earlier_wagons]
 
-def add_missing_stops():
+def add_missing_stops(route: dict, **kwargs) -> dict:
     """Add missing stops to route dict.
 
     :param route: dict - the dict of routing information.
     :param: arbitrary number of stops.
     :return: dict - updated route dictionary.
     """
-    pass
+    route['stops'] = list(kwargs.values())
+    return route
 
 
-def extend_route_information(route, more_route_information):
+def extend_route_information(route: dict[str, str], more_route_information: dict[str, str]) -> dict[str, str]:
     """Extend route information with more_route_information.
 
     :param route: dict - the route information.
     :param more_route_information: dict -  extra route information.
     :return: dict - extended route information.
     """
-    pass
+    return {**route, **more_route_information}
 
 
-def fix_wagon_depot(wagons_rows):
+def fix_wagon_depot(wagons_rows: list[list[tuple[int, str]]]) -> list[list[tuple[int, str]]]:
     """Fix the list of rows of wagons.
 
     :param wagons_rows: list[list[tuple]] - the list of rows of wagons.
     :return: list[list[tuple]] - list of rows of wagons.
     """
-    pass
+    return [list(row) for row in zip(*wagons_rows)]
