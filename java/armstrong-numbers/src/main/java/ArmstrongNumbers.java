52b84f4525e0d4a6c89c2a8ede1a3b6ffe108493
Kendrick Macbook <kendrickyanuar@gmail.com>
2026-02-12 11:49:16 +1100

    done armstrong numbers, iteration 2, wrote method to count number of digits in an int

--- java/armstrong-numbers/src/main/java/ArmstrongNumbers.java
+++ java/armstrong-numbers/src/main/java/ArmstrongNumbers.java
@@ -1,13 +1,23 @@
 class ArmstrongNumbers {
 
+    private int countDigitsIn(int a) {
+        int count = 0;
+        while (a > 0) {
+            a /= 10; // removes last digit
+            count += 1;
+        }
+        return count;
+    }
+
     boolean isArmstrongNumber(int numberToCheck) {
         int numberToCheckCopy = numberToCheck;
-        int numOfDigits = Integer.toString(numberToCheck).length();
-        int sum = 0;
+        int numOfDigits = countDigitsIn(numberToCheck);
+        double sum = 0;
         for (int i = 0; i < numOfDigits; i++) {
             int digit = numberToCheckCopy % 10;
             numberToCheckCopy /= 10; // removes last digit
-            sum += (int) Math.pow(digit, numOfDigits);
+            sum += Math.pow(digit, numOfDigits);
+
         }
         return numberToCheck == sum;
     }
